2a2b74e0b5398648129b0e8012fd998ef75003e6
Harrison Lopes <harrisonlopeslb@gmail.com>
2022-11-26 21:57:53 -0300

    test(DateTool): adding tests

---
+++ test/shared/tools/DateTool.spec.ts
@@ -0,0 +1,23 @@
+import { DateTool } from "~/shared/tools/DateTool";
+
+describe("DateTools", () => {
+  describe("addDaysTo", () => {
+    test.each([
+      ["2022-10-01", "2022-10-11", 10],
+      ["2022-10-01", "2022-09-30", -1],
+      ["2022-02-01", "2022-03-03", 30],
+    ])(
+      "Input %s => Expected: %s",
+      (rawInputDate: string, rawExpectedDate: string, daysToAdd: number) => {
+        const inputDate = new Date(rawInputDate);
+        const expectedDate = new Date(rawExpectedDate);
+
+        console.log(DateTool.addDaysTo(inputDate, daysToAdd));
+
+        expect(DateTool.addDaysTo(inputDate, daysToAdd)).toStrictEqual(
+          expectedDate
+        );
+      }
+    );
+  });
+});
