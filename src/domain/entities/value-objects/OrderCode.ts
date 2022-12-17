449fd67ad4248b16f3397396d047d2c53355a766
Harrison Lopes <harrisonlopeslb@gmail.com>
2022-12-17 11:50:28 -0300

    refactor: adding order sequence size constant

--- src/domain/entities/value-objects/OrderCode.ts
+++ src/domain/entities/value-objects/OrderCode.ts
@@ -6,13 +6,19 @@ interface OrderCodeProps {
 }
 
 export class OrderCode extends ValueObject<OrderCodeProps> {
+  private static ORDER_SEQUENCE_SIZE = 8;
+
   static Create(sequence: Sequence) {
     return new OrderCode({ value: OrderCode.generateCode(sequence) });
   }
 
   private static generateCode(sequence: Sequence) {
     const now = new Date();
-    return `${now.getFullYear()}${String(sequence.value).padStart(8, "0")}`;
+
+    return `${now.getFullYear()}${String(sequence.value).padStart(
+      OrderCode.ORDER_SEQUENCE_SIZE,
+      "0"
+    )}`;
   }
 
   get value(): string {
