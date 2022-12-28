872f3154ec0cce818def95508e218c1c8dad9930
Harrison Lopes <harrisonlopeslb@gmail.com>
2022-12-28 09:21:37 -0300

    refactor(UuidTool): adding method return types

--- src/shared/tools/UuidTool.ts
+++ src/shared/tools/UuidTool.ts
@@ -1,11 +1,11 @@
 import { v4 as uuid, validate, version as uuidVersion } from "uuid";
 
 export class UuidTool {
-  static generate() {
+  static generate(): string {
     return uuid();
   }
 
-  static isValidUuid(uuid: string) {
+  static isValidUuid(uuid: string): boolean {
     return validate(uuid) && uuidVersion(uuid) === 4;
   }
 }
