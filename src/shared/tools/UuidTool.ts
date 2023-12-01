98ed8e9bb6b5c1d1ba1b78e5074b1deb3913d7c0
Harrison Oliveira <harrisonlopeslb@gmail.com>
2023-12-01 14:26:47 -0300

    refactor: use crypto instead of uuid lib for uuid generation

--- src/shared/tools/UuidTool.ts
+++ src/shared/tools/UuidTool.ts
@@ -1,11 +1,14 @@
-import { v4 as uuid, validate, version as uuidVersion } from "uuid";
+import { randomUUID } from "crypto";
 
 export class UuidTool {
+  private static UUIDv4Regex =
+    /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+
   static generate(): string {
-    return uuid();
+    return randomUUID();
   }
 
   static isValidUuid(uuid: string): boolean {
-    return validate(uuid) && uuidVersion(uuid) === 4;
+    return Boolean(uuid.match(this.UUIDv4Regex));
   }
 }
