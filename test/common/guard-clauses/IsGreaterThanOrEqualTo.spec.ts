20f5458ab0b3d039148badc01ab5b77b20b1a04d
Harrison Oliveira <harrisonlopeslb@gmail.com>
2023-12-01 20:48:07 -0300

    refactor: add describe of the method under test

--- test/common/guard-clauses/IsGreaterThanOrEqualTo.spec.ts
+++ test/common/guard-clauses/IsGreaterThanOrEqualTo.spec.ts
@@ -1,27 +1,29 @@
 import { IsGreaterThanOrEqualTo } from "~/common/guard-clauses/clausules";
 
 describe("IsGreaterThanOrEqualTo", () => {
-  describe("should throw when the entered value is lesser than the threshold", () => {
-    it.each([[1], [0]])("%d", (value: number) => {
-      expect(() =>
-        new IsGreaterThanOrEqualTo({
-          value,
-          argumentName: "argument",
-          minValue: 2,
-        }).validate()
-      ).toThrowError(Error);
+  describe("validate", () => {
+    describe("should throw when the entered value is lesser than the threshold", () => {
+      it.each([[1], [0]])("%d", (value: number) => {
+        expect(() =>
+          new IsGreaterThanOrEqualTo({
+            value,
+            argumentName: "argument",
+            minValue: 2,
+          }).validate()
+        ).toThrowError(Error);
+      });
     });
-  });
 
-  describe("should not throw when the entered value is greater than or equal to the threshold", () => {
-    it.each([[2], [3]])("%d", (value: number) => {
-      expect(() =>
-        new IsGreaterThanOrEqualTo({
-          value,
-          argumentName: "argument",
-          minValue: 2,
-        }).validate()
-      ).not.toThrowError();
+    describe("should not throw when the entered value is greater than or equal to the threshold", () => {
+      it.each([[2], [3]])("%d", (value: number) => {
+        expect(() =>
+          new IsGreaterThanOrEqualTo({
+            value,
+            argumentName: "argument",
+            minValue: 2,
+          }).validate()
+        ).not.toThrowError();
+      });
     });
   });
 });
